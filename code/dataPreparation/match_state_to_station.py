44cf91c84c29eb004ca0f37c49aa8ed2f88be084
Luísa- Hörlle de Castro <hoerlledecastro@uni-potsdam.de>
2025-01-14 17:52:29 +0100

    added state data for stations

---
+++ code/dataPreparation/match_state_to_station.py
@@ -0,0 +1,27 @@
+"""
+data from Kaggle too big. provide link TODO
+"""
+
+# import libraries
+import pandas as pd
+import os
+
+
+# load data
+data = pd.read_csv("/Users/luisahorlledecastro/Downloads/DBtrainrides.csv").drop(
+    columns=['ID', 'line', 'path', 'eva_nr', 'category',
+             'zip', 'long', 'lat', 'arrival_plan', 'departure_plan','arrival_change',
+             'departure_change', 'arrival_delay_m', 'departure_delay_m', 'info',
+             'arrival_delay_check', 'departure_delay_check']).drop_duplicates()
+
+df = data[data["state"] == ("Berlin" or "Brandenburg")]
+
+# Directory to save the file
+save_directory = "../../data/cleaned"
+os.makedirs(save_directory, exist_ok=True)  # Ensure the directory exists
+
+# File name and path
+csv_file_name = "station_city_match.csv"
+output = os.path.join(save_directory, csv_file_name)
+
+df.to_csv(output, index=False)
